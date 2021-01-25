cab7511db0132dce4b6b8e6e04bbeb2ceb3b7ac6
Fenguinn <salem.abdulla15@gmail.com>
2021-01-24 20:36:43 -0500

    Item used fixed
    It lowers the value in the inventory when it is confirmed

--- Feel Good Jam/Abdullah Folder/Inventory Things/Bait Prompt.gd
+++ Feel Good Jam/Abdullah Folder/Inventory Things/Bait Prompt.gd
@@ -1,17 +1,21 @@
 extends ColorRect
 
 export (Resource) var inventory
+
 signal bait_confirmed
+var itemM
+var item_indexM
+
 
 func _on_No_pressed():
 	self.visible = false
 
 
 func _on_Yes_pressed():
-#	inventory.stack_item(rand_range(25) ,-1)
+	inventory.stack_item(item_indexM , (-1))
 	self.visible = false
 
-func _on_BaitPrompt_bait_used(item):
-	print("somethoong")
+func _on_BaitPrompt_bait_used(item, item_index):
+	self.itemM = item
+	self.item_indexM = item_index
 	self.visible = true
-

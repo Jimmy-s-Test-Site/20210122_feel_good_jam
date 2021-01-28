47bf06cf182f05655ae613fa47f5998d16858776
Julian <jaacko.torus@gmail.com>
2021-01-28 01:24:34 -0500

    caught fish, die

--- Feel Good Jam/Abdullah Folder/Player Things/Hook.gd
+++ Feel Good Jam/Abdullah Folder/Player Things/Hook.gd
@@ -14,6 +14,7 @@ var rarity_type
 func _on_Hook_area_entered(area):
 	if area.name.find("Fish", 0) != -1:
 		emit_signal("caught_fish", area.item)
+		area.queue_free()
 
 func set_bait_rarity(rarity):
 	self.rarity_type = rarity

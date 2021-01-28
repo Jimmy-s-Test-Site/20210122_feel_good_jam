412bf9d2d8162aef096e03c04c9ac64c270f42e9
Julian <jaacko.torus@gmail.com>
2021-01-28 00:07:57 -0500

    made fish auto despawn

--- Feel Good Jam/Julian/Fish/Fish.gd
+++ Feel Good Jam/Julian/Fish/Fish.gd
@@ -6,5 +6,10 @@ onready var viewport_size = self.get_viewport().size
 export (int) var speed = 100
 
 func _process(delta):
-	if self.position.y < -500:
+	if (
+		self.global_position.x < - 100 or
+		self.global_position.x >  1300 or
+		self.global_position.y < - 100 or
+		self.global_position.y >   900
+	):
 		self.queue_free()
